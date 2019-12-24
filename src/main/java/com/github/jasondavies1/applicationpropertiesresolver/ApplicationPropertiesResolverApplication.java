a7080c27a9fff6f57c1cf11beb97b4ce906b0669
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-12-24 17:43:53 +0000

    first commit

---
+++ src/main/java/com/github/jasondavies1/applicationpropertiesresolver/ApplicationPropertiesResolverApplication.java
@@ -0,0 +1,13 @@
+package com.github.jasondavies1.applicationpropertiesresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPropertiesResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationPropertiesResolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jasondavies1/applicationpropertiesresolver/ApplicationPropertiesResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jasondavies1.applicationpropertiesresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPropertiesResolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
