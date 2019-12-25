31eba66dee358b8b7e7711c1417e922296a3ce93
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-12-25 13:09:00 +0000

    Facilitate download of updated app.env file
    Checkbox to conditionally include property is not binding

--- src/main/java/com/github/jasondavies1/applicationpropertiesresolver/configuration/DocumentController.java
+++ src/main/java/com/github/jasondavies1/applicationpropertiesresolver/configuration/DocumentController.java
@@ -3,15 +3,18 @@ package com.github.jasondavies1.applicationpropertiesresolver.configuration;
 import com.github.jasondavies1.applicationpropertiesresolver.domain.Properties;
 import com.github.jasondavies1.applicationpropertiesresolver.service.MultipartFileToPropertiesConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/document/")
@@ -29,10 +32,32 @@ public class DocumentController {
         return "uploadedFile";
     }
 
-    @GetMapping("modified")
-    public String modified(final Properties properties, final ModelMap modelMap) {
-        modelMap.addAttribute("temp", true);
-        return "home";
+    @GetMapping(value = "modified")
+    @ResponseBody
+    public FileSystemResource modified(
+            final HttpServletResponse response,
+            final Properties propertyForm) throws IOException {
+
+        response.setHeader("Content-Disposition", "attachment; filename=app.env");
+
+        final File temp = File.createTempFile("tmp", ".env");
+        temp.deleteOnExit();
+
+        final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(temp));
+        bufferedWriter.write(propertiesToString(propertyForm));
+        bufferedWriter.close();
+
+        return new FileSystemResource(temp);
+    }
+
+    private String propertiesToString(final Properties properties) {
+        return properties.getProperties().stream()
+                .map(p -> {
+                    final String formattedKey = p.getKey().toUpperCase()
+                            .replaceAll("\\.", "_")
+                            .replaceAll("-", "_");
+                    return String.format("%s=%s", formattedKey, p.getValue());
+                }).collect(Collectors.joining("\n"));
     }
 
 }
